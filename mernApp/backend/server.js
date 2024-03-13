8f690b8e6407717cb4d8b0b7a80f157ac9c35cdc
biruk-tafese <tafesebiruk23@gmail.com>
2024-03-13 16:38:29 +0300

    error handler

--- mernApp/backend/server.js
+++ mernApp/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 
 const app = express()
 
@@ -10,4 +11,5 @@ const port = process.env.PORT || 5000
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 
+app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
